5a7210a01a4f9d1a10e0d2431a8f6689e2e51a79
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-08-08 22:31:43 +0600

    state declared for search

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: "#A2D9CE"
+}
 
 const Header = (props) => {
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            </Container>
         </Navbar>
     );
 }
