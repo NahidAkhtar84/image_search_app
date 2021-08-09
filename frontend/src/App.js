fbffdae089a3a54a1b97be990125fc38e9c40ec6
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-08-09 11:37:51 +0600

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,21 +11,25 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
-      } )
-      .catch((err) => {
-        console.log(err.message)
+        console.log(data);
       })
-  }
-  console.log()
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    //set the search field empty
+    setWord('');
+  };
 
   return (
     <div className="App">
-      <Header title="Image Search"/>
-      <Search word = {word} setWord={setWord} searchKey={handleSearchSubmit}/>
+      <Header title="Image Search" />
+      <Search word={word} setWord={setWord} searchKey={handleSearchSubmit} />
     </div>
   );
 }
