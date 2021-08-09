e73bb6699456595aeeea891da372a79cf7d048c3
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-08-09 13:50:43 +0600

    files modified

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const Search = (props) => {
-    return (
-        <Container className="mt-4">
-            <Row className='justify-content-center'>
-                <Col xs={12} md={8} lg={6}>
-                    <Form onSubmit={props.searchKey}>
-                        <Row>
-                            <Col xs={8}>
-                                <Form.Control
-                                 type="text"
-                                 value={props.word}
-                                 onChange={(e) => props.setWord(e.target.value)}
-                                 placeholder="Search for image here..." 
-                                 />
-                            </Col>
-                            <Col>
-                                <Button variant="primary" type="submit">
-                                    Search
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Col>
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={props.searchKey}>
+            <Row>
+              <Col xs={8}>
+                <Form.Control
+                  type="text"
+                  value={props.word}
+                  onChange={(e) => props.setWord(e.target.value)}
+                  placeholder="Search for image here..."
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
             </Row>
-        </Container>
-    );
-}
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Search;
