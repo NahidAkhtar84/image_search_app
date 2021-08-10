af193fb2983576582dbbdfcc952a0714d74c7d6c
NahidAkhtar84 <nahid.ibne.akhtar@gmail.com>
2021-08-10 11:30:42 +0600

    logo added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
   backgroundColor: '#A2D9CE',
@@ -9,7 +10,7 @@ const Header = (props) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '5rem', maxHeight: '5rem' }} />
       </Container>
     </Navbar>
   );
